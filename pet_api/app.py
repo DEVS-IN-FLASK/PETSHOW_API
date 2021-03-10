caa35b9db55f3e00e5cca10ca25ac3d7586daf14
Gerson <gerson.correia@aluno.faculdadeimpacta.com.br>
2021-03-09 22:22:33 -0300

    Update app.py

--- pet_api/app.py
+++ pet_api/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
+from .model import configure as config_db
 
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////temp/petbd.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/petdb.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    config_db(app)
 
-    return app
+    Migrate(app, app.db)
+     
+    return app 
