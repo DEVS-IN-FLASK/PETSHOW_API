c123a6237254188ab62f06b5949fb19c9924fa7a
Gerson <gerson.correia@aluno.faculdadeimpacta.com.br>
2021-03-16 00:31:57 -0300

    -a

--- pet.py
+++ pet.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://edtjxfihkbbwcg:aafccf72712d5807c4db728c68af8339b0d09bab37c0f090bf95fb6b11e4a4a2@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d7vlr1mc5qqm2r'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://othtixrbfjlhlq:b9a8949c6208ca7d6203efe5e05049f1bfa71d1ba040ed94a23b28bdd5291679@ec2-54-145-102-149.compute-1.amazonaws.com:5432/d6f5ivqn159sv1'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     from usuarios import usuarios_app
