63c89bbcc21dc4d551f0a5f49e27f43dcd540d39
Gerson <gerson.correia@aluno.faculdadeimpacta.com.br>
2021-03-29 21:35:55 -0300

    Adicionada quantidade a tab produtos

--- models/produto.py
+++ models/produto.py
@@ -14,11 +14,12 @@ class Produto(db.Model):
     preco_custo = db.Column(db.Float)
     preco_venda = db.Column(db.Float)
     foto = db.Column(db.Binary)
+    quantidade = db.Column(db.Integer)
     marca_id = db.Column(db.Integer,db.ForeignKey('marcas.id'))
     animal_id = db.Column(db.Integer,db.ForeignKey('animais.id'))
     tamanho_id = db.Column(db.Integer,db.ForeignKey('tamanhos.id'))
 
     def serialize(self):
         return {'id':self.id,'descricao':self.descricao,'modelo':self.modelo,'cod_barras':self.cod_barras, 
-        'porcentagem':self.porcentagem,'preco_custo':self.preco_custo,'preco_venda':self.preco_venda,'foto':self.foto,'marca_id':self.marca_id,'animal_id':self.animal_id,'tamanho_id':self.tamanho_id}
+        'porcentagem':self.porcentagem,'preco_custo':self.preco_custo,'preco_venda':self.preco_venda,'foto':self.foto,'quantidade':self.quantidade,'marca_id':self.marca_id,'animal_id':self.animal_id,'tamanho_id':self.tamanho_id}
 
