23abd84be98dcf4d05aa3cbc5d406e22f96f6cd9
Gerson <gerson.correia@aluno.faculdadeimpacta.com.br>
2021-05-17 21:26:40 -0300

    AtualizacaoConfig

--- petshow_api.py
+++ petshow_api.py
@@ -18,23 +18,10 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-#    app.config['SECRET_KEY'] = 'devsinflaskpetshowapi'
+
     jwt = JWTManager(app)
     
-#    CORS(app, support_credentials=True)
 
-    '''Banco SQlite local'''
-#    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///petdb.sqlite'
-    '''Banco Postgres local'''
-#    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:819246@localhost:5432/petdb'
-
-    '''Banco Postgres Heroku (ativo)'''
-#    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://nuenrexvutummr:e8af86aaf4e99a011914e701532b0fc9bb7b9588b34158cce47e2e921f2ed0c7@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dse9kl9ve57mv'
- #   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    
-    '''URL localhost'''
-#    url = 'http://127.0.0.1:5000/'
-    '''URL Heroku'''
     url = 'https://petshow-api.herokuapp.com/'
 
     from blueprints.usuarios import usuarios_app
